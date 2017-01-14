0bd7faf3e7f5907bf1f24b77b3026504ab437027
airaola <iraola.iribar@gmail.com>
2017-01-14 18:43:28 +0100

    Tarea 6

--- kaixomundua2/static/javascript/AjaxComprobacionEmail.js
+++ kaixomundua2/static/javascript/AjaxComprobacionEmail.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function AjaxComprobacionEmail () {
 	
   var XMLHttpRequestObject = new XMLHttpRequest();
@@ -26,4 +27,34 @@ function AjaxComprobacionEmail () {
 	XMLHttpRequestObject.open ( "get", "/validacion" + correoParaValidacion, true );
     XMLHttpRequestObject.send(null);
   
+=======
+function AjaxComprobacionEmail () {
+	
+  var XMLHttpRequestObject = new XMLHttpRequest();
+  var correoDeAjax = document.getElementById("email");
+  var correoParaValidacion = "?emailAjax=" + correoDeAjax.value;
+  var respuesta;
+
+  if(XMLHttpRequestObject)
+  {
+    XMLHttpRequestObject.onreadystatechange = function()
+    {
+      if (XMLHttpRequestObject.readyState==4)
+      {
+        respuesta = XMLHttpRequestObject.responseText;
+        if (respuesta == ""){
+          document.getElementById("emailBien").innerHTML = ("Email correcto! ");
+          document.getElementById("emailMal").innerHTML= "";
+        }
+        else{
+          document.getElementById("emailBien").innerHTML = "";
+          document.getElementById("emailMal").innerHTML= respuesta;
+        }
+      }
+    }
+  }
+	XMLHttpRequestObject.open ( "get", "/validacion" + correoParaValidacion, true );
+    XMLHttpRequestObject.send(null);
+  
+>>>>>>> 9fbcc993e9244d2631034c3c238f4ddf38fe12df
 }
